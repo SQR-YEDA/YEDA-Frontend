206af4cfca83c4dc9c627a537bf22dae7251f238
e.a.blinov <e.a.blinov@tinkoff.ru>
2024-04-23 20:07:02 +0300

    api-auth

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 import streamlit as st
 from menu import menu
+import requests
+from constants import API
+from streamlit_jwt_authenticator import Authenticator
+
+def handler(data, res):
+    print(data)
+    print(res)
+    return
+
+authenticator = Authenticator(f"{API}/login", response_handler=handler, headers={"Content-Type": "application/json"})
 
 print(st.session_state)
 if "auth" not in st.session_state:
@@ -40,18 +50,34 @@ if st.session_state.auth_form== "sign up":
         st.markdown("###")
         sign_up_submit_button = st.form_submit_button(label = 'Register')
 
-        #if sign_up_submit_button:
-            # Тут запрос на бек...
+        if sign_up_submit_button:
+            data = requests.post(f"{API}/register", json={ 'login': username_sign_up, 'password': password_sign_up })
+
+            if data.status_code == 200:
+                st.session_state.auth_form="sign in"
+            else:
+                st.markdown('ERROR');
 
 if st.session_state.auth_form == "sign in":
-    with st.form("Login Form"):
-        username = st.text_input("Username", placeholder = 'Your unique username')
-        password = st.text_input("Password", placeholder = 'Your password', type = 'password')
+    authenticator.login()
+   
+    if st.session_state["authentication_status"]:
+        st.markdown("Content")
+    
+    # with st.form("Login Form"):
+    #     username = st.text_input("Username", placeholder = 'Your unique username')
+    #     password = st.text_input("Password", placeholder = 'Your password', type = 'password')
 
-        st.markdown("###")
-        login_submit_button = st.form_submit_button(label = 'Login')
+    #     st.markdown("###")
+    #     login_submit_button = st.form_submit_button(label = 'Login')
 
-        # if login_submit_button:
+    #     if login_submit_button:
+    #         data = requests.post(f"{API}/login", json={ 'login': username, 'password': password })
+
+    #         if data.status_code == 200:
+    #             token = data.json()['tokens']['access_token']
+    #         else:
+    #             st.markdown('ERROR');
             
         
 menu()

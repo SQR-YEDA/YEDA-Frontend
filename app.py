6a5203c5020a25b0410d962449e9e0ba2f5d984c
Vyacheslav Rybalchenko <v.rybalchenko@innopolis.university>
2024-04-27 10:41:26 +0300

    fix: lint errors

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ import requests
 from constants import API
 from streamlit_jwt_authenticator import Authenticator
 
+
 def handler(data):
     print("SUCCESS")
     return data.json()['tokens']
+
 
 authenticator = Authenticator(url=f"{API}/login", response_handler=handler, token_key='access_token')
 
@@ -31,8 +33,10 @@ if "auth_form" not in st.session_state:
 
 st.session_state._auth_form = st.session_state.auth_form
 
+
 def set_auth_form():
     st.session_state.auth_form = st.session_state._auth_form
+
 
 st.selectbox(
     "Select auth type:",
@@ -41,23 +45,23 @@ st.selectbox(
     on_change=set_auth_form,
 )
 
-if st.session_state.auth_form== "sign up":
+if st.session_state.auth_form == "sign up":
     with st.form("Sign Up Form"):
-        username_sign_up = st.text_input("Username *", placeholder = 'Enter a unique username')
-        password_sign_up = st.text_input("Password *", placeholder = 'Create a strong password', type = 'password')
+        username_sign_up = st.text_input("Username *", placeholder='Enter a unique username')
+        password_sign_up = st.text_input("Password *", placeholder='Create a strong password', type='password')
 
         st.markdown("###")
-        sign_up_submit_button = st.form_submit_button(label = 'Register')
+        sign_up_submit_button = st.form_submit_button(label='Register')
 
         if sign_up_submit_button:
-            data = requests.post(f"{API}/register", json={ 'login': username_sign_up, 'password': password_sign_up })
+            data = requests.post(f"{API}/register", json={'login': username_sign_up, 'password': password_sign_up})
 
             if data.status_code == 200:
-                st.session_state.auth_form="sign in"
+                st.session_state.auth_form = "sign in"
                 print('authed')
                 st.rerun()
             else:
-                st.markdown('ERROR');
+                st.markdown('ERROR')
 
 if st.session_state.auth_form == "sign in":
     authenticator.login()
@@ -65,5 +69,5 @@ if st.session_state.auth_form == "sign in":
 if st.session_state["authentication_status"]:
     print('LOGOUT')
     authenticator.logout()
-        
+
 menu()
--- widgets.py
+++ widgets.py
@@ -1,3 +1,2 @@
 # class __login__:
 #     def get_login_ui():
-        
